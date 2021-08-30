05f9aa5e2e3cf698b8b15131bf521fb89da4b179
Pawan7778 <pawankumar7209442464@gmail.com>
2021-08-31 02:10:08 +0530

    add next and prev

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -9,16 +9,52 @@ export class News extends Component {
     this.state = {
       articles: this.articles,
       loading: false,
+      page: 1,
     };
   }
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d&page=1";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    this.setState({
+      articles: parsedData.articles,
+      totalArticles: parsedData.totalResults,
+    });
   }
+
+  handlePrevClick = async () => {
+    // console.log("Prev")
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextCLick = async () => {
+    // console.log("Next")
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
@@ -36,6 +72,23 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            Prev
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextCLick}
+          >
+            Next
+          </button>
         </div>
       </div>
     );
