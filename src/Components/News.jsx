6ccc829df26c9b761d97834f41df556fc301c8ba
Pawan7778 <pawankumar7209442464@gmail.com>
2021-09-07 23:14:48 +0530

    Hiding apiKey

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -36,7 +36,7 @@ export class News extends Component {
 
   async updateNews() {
     this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     
     this.props.setProgress(30);
@@ -57,7 +57,7 @@ export class News extends Component {
     // a fake async api call like which sends
     // 20 more records in 1.5 secs
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d8ecc4d7a3904da4b02f9826f2f5fe0d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
